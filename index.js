42f8300c62a20b251b6ac08fe171e582af24e11f
Nat Epstein <nathaniel.epstein@gmail.com>
2022-06-30 15:13:04 -0400

    added event listeners to each button so that an individual sound would be played upon click of button

---
+++ index.js
@@ -0,0 +1,56 @@
+for (let i = 0; i < document.querySelectorAll(".drum").length; i++) {
+    document.querySelectorAll(".drum")[i].addEventListener("click", function () {
+        let buttonHTML = this.innerHTML;
+
+        switch (buttonHTML) {
+            case "w":
+                let tom1 = new Audio('assets/sounds/tom-1.mp3');
+                tom1.play();
+                break;
+
+            case "a":
+                let tom2 = new Audio('assets/sounds/tom-2.mp3');
+                tom2.play();
+                break;
+
+            case "s":
+                let tom3 = new Audio('assets/sounds/tom-3.mp3');
+                tom3.play();
+                break;
+
+            case "d":
+                let tom4 = new Audio('assets/sounds/tom-4.mp3');
+                tom4.play();
+                break;
+            
+            case "j":
+                let snare = new Audio('assets/sounds/snare.mp3');
+                snare.play();
+                break;
+            
+            case "k": 
+                let kick = new Audio('assets/sounds/kick-bass.mp3');
+                kick.play();
+                break;
+            
+            case "l":
+                let crash = new Audio('assets/sounds/crash.mp3');
+                crash.play();
+                break;
+            
+            default: console.log(buttonHTML);
+
+        }
+    })
+}
+
+
+
+
+
+
+
+
+
+
+

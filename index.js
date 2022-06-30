0202fb8d9d1972f41b0e20145104e81735bdc44f
Nat Epstein <nathaniel.epstein@gmail.com>
2022-06-30 16:55:12 -0400

    added animation to buttons

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ for (let i = 0; i < document.querySelectorAll(".drum").length; i++) {
     document.querySelectorAll(".drum")[i].addEventListener("click", function () {
         let buttonInnerHTML = this.innerHTML;
         makeSound(buttonInnerHTML);
+        buttonAnimation(buttonInnerHTML);
        
 
         });
@@ -11,7 +12,8 @@ for (let i = 0; i < document.querySelectorAll(".drum").length; i++) {
 
 // Detecting keydown event
 document.addEventListener("keydown", function(event) {
-    makeSound(event.key)
+    makeSound(event.key);
+    buttonAnimation(event.key);
 })
 
 function makeSound(key) {
@@ -60,4 +62,12 @@ function makeSound(key) {
 
 
 
+function buttonAnimation (currentKey) {
+    let activeButton = document.querySelector("." + currentKey);
+    activeButton.classList.add('pressed');
+
+    setTimeout(function() {
+        activeButton.classList.remove('pressed')
+    }, 100);
+}
 
